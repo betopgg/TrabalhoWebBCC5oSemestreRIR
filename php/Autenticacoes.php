99096468ee231a6b2fa4d78d8748e106127f5015
Dprodigy125 <souzadiogo357@gmail.com>
2022-06-01 19:06:35 -0300

    Autenticação do cadastro

---
+++ php/Autenticacoes.php
@@ -0,0 +1,33 @@
+<?php
+	// abre conexao com o banco 
+	require_once '../BD/Conectabanco.php';
+
+	print_r($_POST);
+	echo "<br><br>";
+	$nome = $_POST["name"];
+	$CPF = $_POST["cpf"];
+	$email = $_POST["email"];
+	$senha = $_POST["senha"];
+    $datanascimento = $_POST["dataN"];
+    $telefone = $_POST["tele"];
+    $cep = $_POST["cep"];
+    
+	$endereco = $_POST["endereco"];
+	$complemento = $_POST["complemento"];
+	$bairro = $_POST["bairro"];
+	$cidade = $_POST["cidade"];
+	$uf = $_POST["uf"];
+
+	$enderecocompleto = $endereco . ", " . $bairro . ", " . $cidade . ", " . $uf . ", " . $complemento; 
+
+
+	//monta sql para o banco de dados ,
+	$sql = "INSERT  INTO  pseudo_dados (NomeCompleto, CPF, Email, Senha, DataNascimento, Telefone, CEP, Endereco)  VALUES ('" . $nome . "','" . $CPF . "','" . $email . "','" . $senha . "','" . $datanascimento . "','" . $telefone . "','" . $enderecocompleto . "')";
+	echo $sql;
+	echo "<br><br>";
+
+	$stmt = $pdo->prepare($sql);
+	$stmt->execute();
+
+	header('Location: .../index.html');
+?>
